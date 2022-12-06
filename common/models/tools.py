65e1268c9702268acebbccbfe6db91da72f3db48
SZ\yansha <yansha@rayvision.com>
2022-12-06 10:24:51 +0800

    pets

--- common/models/tools.py
+++ common/models/tools.py
@@ -3,7 +3,7 @@ from .base import BaseModel
 
 
 class Tag(BaseModel):
-    __tablename__  = 'tb_tag'
+    __tablename__ = 'tb_tag'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     type = db.Column(db.String(256))
