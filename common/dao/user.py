c870c875ff1b345b403c23c5a44e9a8f441ee552
nonoroazoro25 <814472228@qq.com>
2022-12-08 17:00:29 +0800

    update user dao

--- common/dao/user.py
+++ common/dao/user.py
@@ -12,3 +12,34 @@ def get_user_all():
         db.session.rollback()
         raise DatabaseError
     return info
+
+def get_user_by_user_id(user_id):
+    try:
+        info = User.query.filter_by(id=user_id).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        raise DatabaseError
+    return info
+
+
+def update_user_by_user_id(user_id, update_dict):
+    try:
+        User.query.filter_by(id=user_id).update(update_dict)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return False
+    return True
+
+
+def delete_user_by_user_id(user_id):
+    try:
+        User.query.filter_by(id=user_id).delete()
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        raise DatabaseError
+    return True
